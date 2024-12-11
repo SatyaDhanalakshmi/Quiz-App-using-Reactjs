7a8e44f786fde027bb460ee0c589816ce334d3ed
SatyaDhanalakshmi <satya.teche@gmail.com>
2024-12-11 13:25:23 +0530

    initial commit

---
+++ QuestionBank.js
@@ -0,0 +1,27 @@
+export const Questions=[
+      {
+        prompt:"what is React?",
+        optionA:"Language",
+        optionB:"Framwork",
+        optionC:"Library",
+        optionD:"Programming Language",
+        answer:"C",
+      },
+      {
+        prompt:" What is Virtual DOM?",
+        optionA:"Virtual DOM same as Real DOM",
+        optionB:"Lightweight Copy of the Actual DOM",
+        optionC:"Browser technology  ",
+        optionD:"Actual Dom",
+        answer:"B",
+      },
+      {
+        prompt:"what is the purpose useState Hook?",
+        optionA:"For Managing State",
+        optionB:"For Handle side Effects",
+        optionC:"For Consuming Context",
+        optionD:" For Maintaining mutable  references",
+        answer:"A",
+      },
+
+]
