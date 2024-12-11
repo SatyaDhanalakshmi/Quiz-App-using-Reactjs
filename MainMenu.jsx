75c0a5bc8e7ecaf159ae9302b00faddcea3580bf
SatyaDhanalakshmi <satya.teche@gmail.com>
2024-12-11 13:26:17 +0530

    initial commit

---
+++ MainMenu.jsx
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react';
+import {QuizContext} from '../Helpers/Context';
+import  "../App.css"
+function MainMenu() {
+    const {gameState ,setGameState}=useContext(QuizContext);
+  return (
+    <div className='Menu'>
+      <h1>{gameState}</h1>
+      <button onClick={()=>{setGameState("quiz");}}>Start Quiz</button>
+    </div>
+  )
+}
+
+export default MainMenu;
